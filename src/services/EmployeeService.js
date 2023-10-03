83c3fb44067847e1a1d761b3969805df9458e898
Chayan Barman <barmanchayan10@gmail.com>
2023-10-04 03:23:02 +0530

    Added comment

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const EMP_ADD_URL = "http://localhost:8080/api/v1/addEmp"; 
 const EMP_GET_URL = "http://localhost:8080/api/v1/getEmp";
 const EMP_DELETE_URL = "http://localhost:8080/api/v1/deleteEmp"; 
+
+//The file which handles the API 
+
 class EmployeeService{
       saveEmployee(employee){
         return axios.post(EMP_ADD_URL, employee); 
